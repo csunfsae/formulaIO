bcb901ce5fe195c632b75d433d0230416c467ec6
Alejandro Martinez <hello@alejandrom.xyz>
2018-11-05 07:17:59 -0800

    Removed Post Routes
    Signed-off-by: Alejandro Martinez <hello@alejandrom.xyz>

--- socket/routes/api.js
+++ socket/routes/api.js
@@ -9,42 +9,17 @@ router.get('/health', (req, res) => {
     res.json(health);
   });
 });
-router.post('/health', (req, res) => {
-  const time = Date.parse(req.body.time);
-  models.Health.create({
-    time,
-    active: req.body.active,
-    device: req.body.device,
-  }).then(res.status(200).end());
-});
 
 router.get('/pressure', (req, res) => {
   models.Pressure.findAll().then((pressure) => {
     res.json(pressure);
   });
 });
-router.post('/pressure', (req, res) => {
-  const time = Date.parse(req.body.time);
-  models.Pressure.create({
-    time,
-    value: req.body.value,
-    device: req.body.device,
-  }).then(res.status(200).end());
-});
 
 router.get('/traction', (req, res) => {
   models.Traction.findAll().then((traction) => {
     res.json(traction);
   });
-});
-router.post('/traction', (req, res) => {
-  const time = Date.parse(req.body.time);
-  models.Traction.create({
-    time,
-    value: req.body.value,
-    is_slip: req.body.is_slip,
-    device: req.body.device,
-  }).then(res.status(200).end());
 });
 
 
