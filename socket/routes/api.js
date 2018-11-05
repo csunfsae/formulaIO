880f1111238e3bc9412fa66344f18d6ef8fd51a5
Alejandro Martinez <hello@alejandrom.xyz>
2018-11-05 07:14:56 -0800

    Get Routes
    Signed-off-by: Alejandro Martinez <hello@alejandrom.xyz>

--- socket/routes/api.js
+++ socket/routes/api.js
@@ -1,32 +1,50 @@
 const express = require('express');
+
 const router = express.Router();
 
-var models  = require('../models');
+const models = require('../models');
 
-/* {PST health }. */
 router.get('/health', (req, res) => {
-    models.Health.findAll().then(function(health) {
+  models.Health.findAll().then((health) => {
     res.json(health);
   });
 });
 router.post('/health', (req, res) => {
-    let time = Date.parse(req.body.time); 
-    models.Health.create({
-        time:time,
-        active:req.body.active,
-        device:req.body.device
-    }).then(res.status(200).end());
+  const time = Date.parse(req.body.time);
+  models.Health.create({
+    time,
+    active: req.body.active,
+    device: req.body.device,
+  }).then(res.status(200).end());
 });
 
 router.get('/pressure', (req, res) => {
-    models.Pressure.findAll().then(function(pressure) {
+  models.Pressure.findAll().then((pressure) => {
     res.json(pressure);
   });
 });
+router.post('/pressure', (req, res) => {
+  const time = Date.parse(req.body.time);
+  models.Pressure.create({
+    time,
+    value: req.body.value,
+    device: req.body.device,
+  }).then(res.status(200).end());
+});
+
 router.get('/traction', (req, res) => {
-    models.Traction.findAll().then(function(traction) {
+  models.Traction.findAll().then((traction) => {
     res.json(traction);
   });
+});
+router.post('/traction', (req, res) => {
+  const time = Date.parse(req.body.time);
+  models.Traction.create({
+    time,
+    value: req.body.value,
+    is_slip: req.body.is_slip,
+    device: req.body.device,
+  }).then(res.status(200).end());
 });
 
 
