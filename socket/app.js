840fe6a33813026f7c0c00734ca5c26277c555eb
Alejandro Martinez <hello@alejandrom.xyz>
2018-11-30 18:50:25 -0800

    Completed Socket IO implementation

--- socket/app.js
+++ socket/app.js
@@ -1,16 +1,18 @@
 const createError = require('http-errors');
 const express = require('express');
+const socketIO = require('socket.io');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const io = socketIO();
+app.io = io;
+
 const apiRouter = require('./routes/api');
+require('./routes/io')(io);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,9 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/api', apiRouter);
-app.use('/users', usersRouter);
+app.use('/', apiRouter.router);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ socket/routes/io.js
@@ -0,0 +1,25 @@
+const api = require('./api');
+
+function ioRouter(io) {
+  io.on('connection', (socket) => {
+    socket.on('voltage', (data) => {
+      api.createVoltage(data);
+    });
+    socket.on('offsets', (data) => {
+      api.createOffsets(data);
+    });
+    socket.on('speed', (data) => {
+      api.createSpeed(data);
+    });
+    socket.on('health', (data) => {
+      api.createHealth(data);
+    });
+    socket.on('pressure', (data) => {
+      api.createPressure(data);
+    });
+    socket.on('traction', (data) => {
+      api.createTraction(data);
+    });
+  });
+}
+module.exports = ioRouter;
